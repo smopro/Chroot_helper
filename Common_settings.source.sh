2c3a3e1312d8f76a947359e5d0f1c1185ed5a3c0
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2013-10-09 04:53:27 +0800

    復原 commit 1204845 的部份修改

--- Common_settings.source.sh
+++ Common_settings.source.sh
@@ -1,7 +1,7 @@
 # Chroot_helper common settings
 # 	This file contains setting variable used by Chroot_filesystem.sh and Unchroot_filesystem.sh and needed to be edited before running them.
 ## The filesystem to chroot
-CHROOT_TARGET="/dev/sda3"
+CHROOT_TARGET="/dev/device_file?"
 
 ## The directory name to mount the filesystem at
 CHROOT_MOUNTPOINT_NAME="Chrooted_filesystem_root_directory"
