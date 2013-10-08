44758cc509feb73ce3160923cc59c2c7e1ab72c8
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2013-10-09 04:43:43 +0800

    更換成新版本的 header 格式

--- Chroot_filesystem.sh
+++ Chroot_filesystem.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 # 上一句用來宣告執行script用的shell
 ####################################################
-# Script程式名稱 | Script Name
-# 	Chroot_filesystem.sh
-# 著作權宣告 | Copyright Declaration
-# 	Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2012
-# 授權條款宣告 | License Declaration
+# Script程式名稱
+# Script Name
+# 	Script to chroot into a target system
+# 著作權宣告
+# Copyright Declaration
+# 	Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2012, 2013
+# 授權條款宣告
+# License Declaration
 # 	GNU Lesser General Public License version 3.0 or later
 # 	http://www.gnu.org/licenses/lgpl.html
-# 傳回值定義 | Return Value Definition
+# 傳回值定義
+# Return Value Definition
 # 	0
 # 		正常結束
-# 已知問題 | Known Issues
-# 	Known issues is now on GitHub
+# 已知問題
+# Known Issues
+# 	Known issues is now tracked on GitHub
 # 	
-# 修訂紀錄 | Changelog
-# 	Changelog is now on GitHub
+# 修訂紀錄
+# Changelog
+# 	Changelog is now tracked using Git repostiory commit log
 # 	
 ###################################################
 # Load common settings
