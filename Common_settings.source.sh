9bc7651123694ac9c9fa1cf5fb822ddadda25362
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2013-10-09 02:51:34 +0800

    以下變更：
    * 加入檔案標題說明文字
    * 改善目標檔案系統掛載目錄名稱可理解性
    * 修改預設目標檔案系統所在裝置檔案名稱，避免使用者誤操作

--- Common_settings.source.sh
+++ Common_settings.source.sh
@@ -1,8 +1,10 @@
-# The partition to chroot
-CHROOT_PARTITION="/dev/sdb1"
+# Chroot_helper common settings
+# 	This file contains setting variable used by Chroot_filesystem.sh and Unchroot_filesystem.sh and needed to be edited before running them.
+## The partition to chroot
+CHROOT_PARTITION="/dev/device_file?"
 
-# The directory name to mount the partition at
-CHROOT_DIRECTORY_NAME="Chrooted_root"
+## The directory name to mount the partition at
+CHROOT_DIRECTORY_NAME="Chrooted_filesystem_root_directory"
 
-# Mount options to mount the chrooted partition
-CHROOT_MOUNT_OPTION="-o suid,exec,rw"
+## Mount options to mount the chrooted partition
+CHROOT_MOUNT_OPTION="-o suid,exec,dev,rw"
