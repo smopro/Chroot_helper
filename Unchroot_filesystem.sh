451480e5c55abe0c78b408d3ee331ff5a5e693f5
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2013-10-09 03:00:33 +0800

    header 中項目名稱改用中英文雙行型式

--- Unchroot_filesystem.sh
+++ Unchroot_filesystem.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 # 上一句用來宣告執行script用的shell
 ####################################################
-# Script程式名稱 | Script Name
+# Script程式名稱
+# Script Name
 # 	unchroot_filesystem.sh
-# 著作權宣告 | Copyright Declaration
+# 著作權宣告
+# Copyright Declaration
 # 	Ｖ字龍(Vdragon) <pika1021@gmail.com> (c) 2012
-# 授權條款宣告 | License Declaration
+# 授權條款宣告
+# License Declaration
 # 	GNU Lesser General Public License version 3.0 or later
 # 	http://www.gnu.org/licenses/lgpl.html
-# 傳回值定義 | Return Value Definition
+# 傳回值定義
+# Return Value Definition
 # 	0
 # 		正常結束
-# 已知問題 | Known Issues
+# 已知問題
+# Known Issues
 # 	Known issues is now on GitHub
 # 	
-# 修訂紀錄 | Changelog
+# 修訂紀錄
+# Changelog
 # 	Changelog is now on GitHub
 # 	
 ###################################################
